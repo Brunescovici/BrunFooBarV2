e1f7188413eecc0fedd73eb8f161b6f0a0abcbf1
Brunescovici <andrei.pricope0@gmail.com>
2021-08-27 16:35:53 +0200

    scripted clickable navItems

--- script.js
+++ script.js
@@ -45,5 +45,34 @@ function workData(x) {  // x is the data got from the database
     });
 }
 
+// Navigation Script
 
+const navDashboard = document.querySelector("#dashboardNavElement");
+const navProfile = document.querySelector("#profileNavElement");
+const navCalendar = document.querySelector("#calendarNavElement");
 
+navDashboard.addEventListener('click', function() {
+    selectItem("dash");
+});
+navProfile.addEventListener('click', function() {
+    selectItem("profile");
+});
+navCalendar.addEventListener('click', function() {
+    selectItem("calendar");
+})
+
+function selectItem(x) {
+    navDashboard.classList.remove("selectedNavItem");
+    navProfile.classList.remove("selectedNavItem");
+    navCalendar.classList.remove("selectedNavItem");
+    console.log("it works");
+    if(x=="dash") {
+        navDashboard.classList.add("selectedNavItem");
+    }
+    else if(x=="profile") {
+        navProfile.classList.add("selectedNavItem");
+    }
+    else {
+        navCalendar.classList.add("selectedNavItem");
+    }
+}
